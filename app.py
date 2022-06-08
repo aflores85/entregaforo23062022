7e91ba4743118974d9df486b5cfb9041dd372f4c
aflores85 <aflores85@gmail.com>
2022-06-08 19:10:21 -0300

    main

--- app.py
+++ app.py
@@ -1,25 +1,13 @@
-import json
-from flask import Flask, request, jsonify # diccionarios de python los convienrte en Json y en response, objeto request
+from flask import Flask, request, jsonify  # diccionarios de python los convienrte en Json y en response, objeto request
 from flask_sqlalchemy import SQLAlchemy # libreria para BBDD - ORM mapeos de objetos a una base Relacional
 from flask_cors import CORS # por seguridad Cros Origin
-from routers.routers import get_posts
-from routers.routers import get_subject
-from routers.routers import new_post
-from routers.routers import new_subject
-from routers.routers import new_foro
-from routers.routers import delete_posts
-from routers.routers import delete_subject
-from routers.routers import get_foro
+from flask import Blueprint 
+from venv import create
+from flask_cors import CORS # por seguridad Cros Origin
+
 # initialize app
 app = Flask(__name__) #creamos el objeto, un servidor web, responde peticiones de request
-app.register_blueprint(get_foro)
-app.register_blueprint(get_posts)
-app.register_blueprint(get_subject)
-app.register_blueprint(new_post)
-app.register_blueprint(new_subject)
-app.register_blueprint(new_foro)
-app.register_blueprint(delete_posts)
-app.register_blueprint(delete_subject)
+
 
 
 if __name__ == "__main__":
@@ -31,5 +19,134 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres:admin@lo
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+class Foro(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    content = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return '<Foro %r>' % self.title
+
+class Subject(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    content = db.Column(db.String(80), nullable=False)
+    foro_id = db.Column(db.Integer, db.ForeignKey('foro.id'), nullable=False)
+    foro = db.relationship('Foro', 
+    backref=db.backref('subjects', lazy=True))
+
+    def __repr__(self):
+        return '<Subject %r>' % self.title
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    content = db.Column(db.String(80), nullable=False)
+    image_url = db.Column(db.String(180), nullable=False)
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False)
+    subject = db.relationship('Subject', 
+    backref=db.backref('posts', lazy=True))
+
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
+
+@app.route('/api/v1/newforo', methods=['POST'])
+def new_foro():
+    foro_to_create = Foro(title=request.json['title'],
+                          content=request.json['content']
+                                                        )
+
+    db.session.add(foro_to_create)
+    db.session.commit()
+    return jsonify({'message': 'Foro created successfully'})
+
+
+@app.route('/api/v1/newsubject', methods=['POST'])
+def new_subject():
+    subject_to_create = Subject(title=request.json['title'],
+                          content=request.json['content'],
+                          foro_id=request.json['foro_id'])
+
+    db.session.add(subject_to_create)
+    db.session.commit()
+    return jsonify({'message': 'Subject created successfully'})
+
+
+@app.route('/api/v1/newpost', methods=['POST'])
+def new_post():
+    post_to_create = Post(title=request.json['title'],
+                          content=request.json['content'],
+                          image_url=request.json['imageURL'],
+                          subject_id=request.json['subjectid'])
+
+    db.session.add(post_to_create)
+    db.session.commit()
+    return jsonify({'message': 'Post created successfully'})
+
+@app.route('/api/v1/getforo', methods=['GET'])
+def get_foro():
+    foros_Query = Foro.query.all() 
+    foros_list = []
+    for foros in foros_Query:
+        foros_list.append(
+            {'title': foros.title,
+             'id': foros.id,
+             'content': foros.content,          
+             })
+    return jsonify({'foros': foros_list, 'message': 'foros fetched successfully'})
+
+@app.route('/api/v1/getsubject', methods=['GET'])
+def get_subject():    
+    subject_Query = Subject.query.all()
+    subject_list = []
+    for subject in subject_Query:
+        subject_list.append(
+            {'title': subject.title,
+             'id': subject.id,
+             'content': subject.content,
+             'foro_id': subject.foro_id
+            })
+    return jsonify({'Subject': subject_list, 'message': 'subject fetched successfully'})
+
+@app.route('/api/v1/getPosts', methods=['GET'])
+def get_posts():
+    # current_app.logger.info('request')
+    posts = Post.query.all()
+    posts_list = []
+    for post in posts:
+        posts_list.append(
+            {'title': post.title,
+             'id': post.id,
+             'content': post.content,
+             'image': post.image_url
+             })
+    return jsonify({'posts': posts_list, 'message': 'Posts fetched successfully'})
+
+#los deletes
+
+@app.route('/api/v1/deletepost/<id>', methods=['DELETE'])
+def delete_posts(id):
+    query_post_to_delete = Post.query.filter_by(id=id).first()
+    if query_post_to_delete is None:
+        return jsonify({'message': 'Post does not exists'}), 404
+    db.session.delete(query_post_to_delete)
+    db.session.commit()
+    return jsonify({'message': 'Posts fetched successfully'})
+
+
+@app.route('/api/v1/deletesubject/<id>', methods=['DELETE'])
+def delete_subject(id):
+    query_subject_to_delete = Subject.query.filter_by(id=id).first()
+    if query_subject_to_delete is None:
+        return jsonify({'message': 'Subject does not exists'}), 404
+
+    db.session.delete(query_subject_to_delete)
+    db.session.commit()
+    return jsonify({'message': 'Subject fetched successfully'})
+
+
+
 db.create_all()
 
