12ad2d652d98250a1ff5ce0c574fe432f9491824
aflores85 <aflores85@gmail.com>
2022-06-13 19:27:17 -0300

    main

---
+++ quasar-project/src/layouts/StartingMainLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+  <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+export default {
+  name: 'StartingMainLayout',
+  
+}
+
+</script>
